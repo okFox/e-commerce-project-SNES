8b9f7f1446fed2f741e5d31f64938cf6dc56f12b
okFox <smayfield@gmail.com>
2019-10-07 17:58:11 -0700

    basic structure and logic ready to test

---
+++ data/carts.js
@@ -0,0 +1,37 @@
+const shadowrun = {
+    id: 'shadowrun',
+    name: 'Shadowrun SNES',
+    image: 'shadowrun.jpg',
+    description: 'The greatest SNES game of all time.',
+    category: 'adventure',
+    price: 100.00,
+    cost: 75.00
+};
+
+const secretOfMana = {
+    id: 'mana',
+    name: 'Secret of Mana SNES',
+    image: 'mana.jpeg',
+    description: 'A young warrior must undertake a dangerous journey to find the seeds of the Mana tree which have been hidden for centuries.',
+    category: 'adventure',
+    price: 90.00,
+    cost: 50.00
+};
+
+const marioKart = {
+    id: 'marioKart',
+    name: 'Super Mario Kart',
+    image: 'SMK.jpeg',
+    description: 'With more than 15 tracks to master and nearly endless replay value, Super Mario Kart is classic gaming…with some banana peels thrown in for good measure!',
+    category: 'racing',
+    price: 60.00,
+    cost: 25.00
+};
+
+const cartList = [
+    shadowrun,
+    secretOfMana,
+    marioKart
+];
+
+export default cartList;
