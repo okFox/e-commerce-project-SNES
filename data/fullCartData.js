b3f53869cb3197a20ebe8af4ca5ae37ecb4f3d81
okFox <smayfield@gmail.com>
2019-10-08 17:27:48 -0700

    added button.  added array with customer order

---
+++ data/fullCartData.js
@@ -0,0 +1,18 @@
+//an array of items simulating the customer's order
+
+
+
+
+
+
+export const fullCartData = [{
+    id: 'shadowrun',
+    quantity: 2
+}, {
+    id: 'mana',
+    quantity: 3
+}, {
+    id: 'marioKart',
+    quantity: 1
+
+}];
