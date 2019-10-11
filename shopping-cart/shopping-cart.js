3dbd4f79255d76e532e8ac36c498bf8ed7c8fb9d
okFox <smayfield@gmail.com>
2019-10-11 11:36:02 -0700

    place order button working

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -3,10 +3,9 @@ import renderShoppingCart from './render-shopping-cart.js';
 import gameList from '../data/games.js';
 import { toUSD, findById, calcOrderTotal } from '../common/utils.js';
 
-
-
 let checkoutbasket = localStorage.getItem('working-basket');
 let serializedCheckoutBasket = JSON.parse(checkoutbasket);
+const placeOrderButton = document.getElementById('place-order-button');
 
 
 
@@ -26,6 +25,14 @@ for (let i = 0; i < serializedCheckoutBasket.length; i++) {
     tbody.appendChild(dom);
 
 }
+
+
+placeOrderButton.addEventListener('click', () => {
+    localStorage.removeItem('working-basket');
+    let serializedCheckoutBasket = JSON.parse(checkoutbasket);
+    alert('Ready to Checkout?\n' + JSON.stringify(serializedCheckoutBasket, true, 2));
+    window.location = '../';
+});
 
 
 document.getElementById('place-order-button').removeAttribute('disabled');
